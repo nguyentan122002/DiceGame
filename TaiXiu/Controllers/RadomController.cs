1659a56c22abdc121bc01dad02014a73a6f5cce2
Your Name <you@example.com>
2025-02-16 00:08:04 +0700

    add code

---
+++ TaiXiu/Controllers/RadomController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaiXiu.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RadomController : ControllerBase
+    {
+        [HttpGet("roll-dice")]
+        public IActionResult GetRandomNumbers()
+        {
+            try
+            {
+                Random random = new Random();
+                var numbers = new int[3];
+                int total = 0;
+                string result;
+
+                for (int i = 0; i < 3; i++)
+                {
+                    numbers[i] = random.Next(1, 7); 
+                    
+                }
+
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    total = total + numbers[i];
+                }
+
+                if (total < 11)
+                {
+                    result = "Xỉu";
+                }
+                else
+                {
+                    result = "Tài";
+                }
+
+                return Ok(new { DiceRolls = numbers, Total = total, Result = result }) ;
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { Message = "Lỗi hệ thống", Error = ex.Message });
+            }
+        }
+
+        
+    }
+}
